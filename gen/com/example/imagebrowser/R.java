3a3bc83f13c073b94d588331d3947bf5e5db6806
zhangshaoru <zhangshaoru@cvte.cn>
2013-04-28 20:16:35 +0800

    [supplement]

--- gen/com/example/imagebrowser/R.java
+++ gen/com/example/imagebrowser/R.java
@@ -22,6 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pic1=0x7f020001;
+        public static final int pic2=0x7f020002;
+        public static final int pic3=0x7f020003;
+        public static final int pic4=0x7f020004;
+        public static final int pic5=0x7f020005;
+        public static final int pic6=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
